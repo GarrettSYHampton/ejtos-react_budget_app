24d4d963087c198b0c252b3095773f53c1fe52c3
Garrett Hampton <g@garretthampton.com>
2023-12-22 13:52:33 -0500

    Updated work

--- src/App.js
+++ src/App.js
@@ -6,34 +6,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import CurrencyPicker from './components/CurrencyPicker';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
 import AllocationForm from './components/AllocationForm';
 
 import { AppProvider } from './context/AppContext';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <Budget />
-                    </div>     
+                    </div>
                     <div className='col-sm'>
                         <Remaining />
-                    </div>    
+                    </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
-                    </div>    
+                    </div>
+                    <div className='col-sm'>
+                        <CurrencyPicker />
+                    </div>
+                </div>
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <ExpenseList />
-                    </div>    
-                    <div className='col-sm'>
-                        <ExpenseItem />
-                    </div>    
+                    </div>
+                </div>
+                <h2 className='mt-3'>Create New Allocation</h2>
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <AllocationForm />
                     </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { TiDelete, TiMinus, TiPlus } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -25,12 +25,40 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>
+                {props.name}
+            </td>
+            <td>
+                {props.currency}{props.cost}
+            </td>
+            <td>
+                <TiPlus color='green' size='1.5em' onClick={event => increaseAllocation(props.name)}>
+                    +
+                </TiPlus>
+            </td>
+            <td>
+                <TiMinus color='red' size='1.5em' onClick={event => decreaseAllocation(props.name)}>
+                    -
+                </TiMinus>
+            </td>
+            <td>
+                <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
+            </td>
         </tr>
     );
 };
